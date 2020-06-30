453e278c5114f70cd21c1f5cf58ef212e83a5c22
unknown <tiagocvalim@gmail.com>
2020-06-30 09:36:56 -0300

    Conteúdo da aula completo

--- Produto.cs
+++ Produto.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Aulas_27_28_29_30
@@ -34,6 +35,43 @@ namespace Aulas_27_28_29_30
 
         }
 
+        public List<Produto> Ler()
+        {
+            // Criamos lista para guardar o retorno
+            List<Produto> prod = new List<Produto>();
+
+            // Lemos o .csv e separamos um array de linhas 
+            string[] linhas = File.ReadAllLines(PATH);
+
+            // Varremos nossas linhas
+            foreach(string linha in linhas)
+            {
+                // codigo=1;nome=Gibson;preco=5500
+                string[] dado = linha.Split(";");
+
+                Produto p = new Produto();
+                p.Codigo = Int32.Parse( dado[0].Split("=")[1] );
+                p.Nome = dado[1].Split("=")[1];
+                p.Preco = float.Parse( dado[2].Split("=")[1] );
+
+                prod.Add(p);
+            }
+            
+            return prod;
+        }
+
+        /// <summary>
+        /// Método que separa o simbolo = da string do csv
+        /// </summary>
+        /// <param name="dado">Coluna do csv separada</param>
+        /// <returns>string apenas com o valor da coluna</returns>
+        public string Separar(string dado)
+        {
+            // codigo=1
+            // [0]codigo
+            // [1]1
+            return dado.Split("=")[1];
+        }
 
         //  1   ; sapato ; 34,99
         //Codigo; Produto; Preço
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Aulas_27_28_29_30
 {
@@ -7,16 +8,20 @@ namespace Aulas_27_28_29_30
         static void Main(string[] args)
         {
             Produto p1 = new Produto();
-            p1.Codigo = 3;
-            p1.Nome = "Nike Dominant";
-            p1.Preco = 120f;
+            p1.Codigo = 2;
+            p1.Nome = "Sprite";
+            p1.Preco = 4.5f;
 
             p1.Cadastrar(p1);
 
+            List<Produto> lista = p1.Ler();
+            
+            foreach (Produto item in lista)
+            {
+                Console.WriteLine( $"R$:{item.Preco} - {item.Nome}" );
+            }
 
-            
-            
-            
+
         }
     }
 }
